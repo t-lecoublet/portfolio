ed928a63114a73daa58cffcb78f5da3d51ae0fb8
Theo LECOUBLET <theo.lecoublet@uca.fr>
2025-03-07 11:28:29 +0100

    Add fixed background

--- project/app/layouts/default.vue
+++ project/app/layouts/default.vue
@@ -1,201 +1,137 @@
 <template>
-    <svg>
-        <filter id="noiseFilter">
-            <feTurbulence type="turbulence" baseFrequency="0.05" numOctaves="4" seed="15" stitchTiles="stitch" x="0%"
+    <div class="fixed">
+        <div class="background-effect">
+            <img class="v1" src="/layout/Vector 1.svg" />
+            <img class="v2" src="/layout/Vector 2.svg" />
+            <img class="v3" src="/layout/Vector 3.svg" />
+            <img class="v4" src="/layout/Vector 4.svg" />
+            <img class="v6" src="/layout/Vector 6.svg" />
+        </div>
+    </div>
+
+    <svg class="svgFilter">
+        <filter id="nnnoise-filter">
+            <feTurbulence type="turbulence" baseFrequency="0.2" numOctaves="4" seed="15" stitchTiles="stitch" x="0%"
                 y="0%" width="100%" height="100%" result="turbulence"></feTurbulence>
-            <feSpecularLighting surfaceScale="40" specularConstant="1" specularExponent="20" lighting-color="#080c14"
+            <feSpecularLighting surfaceScale="18" specularConstant="0.4" specularExponent="20" lighting-color="#111e38"
                 x="0%" y="0%" width="100%" height="100%" in="turbulence" result="specularLighting">
                 <feDistantLight azimuth="3" elevation="200"></feDistantLight>
             </feSpecularLighting>
-
         </filter>
     </svg>
-    <div class="yellow blob"></div>
-    <div class="red blob"></div>
-    <div class="green blob"></div>
+
     <slot />
 
 </template>
 
 <style>
-:root {
-    --bg: #152032;
-    --text: #151515;
-    --yellow: #152032;
-    --red: #4F0562;
-    --green: #152032;
+
+.svgFilter {
+    height: 0;
 }
 
-
-body {
-    height: 100vh;
-    margin: 0;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 3em;
-
-    background-color: var(--bg);
-    font-family: "Recoleta";
-
-    margin: 0 10em;
-}
-
-h1 {
-    font-size: 60px;
-    font-weight: normal;
-    margin: 0;
-}
-
-h2 {
-    font-size: 32px;
-}
-
-p {
-    font-size: 20px;
-}
-
-a {
-    text-decoration: none;
-    color: inherit;
-}
-
-body::before,
-body::after {
-    position: absolute;
-    left: 0;
+.fixed {
+    position: fixed;
     top: 0;
-    content: '';
+    left: 0;
     width: 100%;
     height: 100%;
-    z-index: -1;
+    z-index: -3;
 
-    opacity: 40%;
+    /* background-color: #080C14; */
 }
 
-body::before {
-    background: #8C8C8C;
-    filter: url(#noiseFilter);
+.background-effect {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    z-index: -3;
+    overflow: clip;
+
+    background-color: #080C14;
+
+    &::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: hsla(220, 43%, 5%, 0.50);
+        backdrop-filter: blur(20px);
+
+        z-index: -2;
+    }
+
+    &::before {
+        z-index: 1;
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: #111e38;
+        filter: url(#nnnoise-filter);
+    }
 }
 
-
-
-
-.blob {
-    border-radius: 100px;
-    filter: blur(60px);
-}
-
-.yellow {
-    background-color: var(--yellow);
+.v1 {
     position: absolute;
-    top: 200px;
-    left: 100px;
-    height: 200px;
-    width: 200px;
+    top: 0px;
+    left: 0px;
+    height: 100%;
+    filter: brightness(2);
+    z-index: -3;
 
-    animation: yellow 8s infinite ease;
+    @media (max-width: 768px) {
+        height: 50vh;
+        top: -10vw
+    }
 }
 
-.green {
-    background-color: var(--green);
+.v2 {
     position: absolute;
-    top: 80px;
-    right: -20px;
-    height: 200px;
-    width: 250px;
+    bottom: -25%;
+    left: 0;
+    width: 50vw;
+    filter: brightness(2);
+    z-index: -3;
 
-    animation: green 8s infinite ease;
+    @media (max-width: 768px) {
+        width: 100vw;
+        bottom: -10vw;
+    }
 }
 
-.red {
-    background-color: var(--red);
+.v3 {
     position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    filter: brightness(2);
+    z-index: -3;
+
+}
+
+.v4 {
+    position: absolute;
+    top: 0px;
+    right: -50vw;
+
+    filter: brightness(2);
+    z-index: -3;
+
+}
+
+.v6 {
+    position: absolute;
+    top: 50%;
     right: 0;
-    top: 300px;
-    height: 250px;
-    width: 200px;
+    transform: translateY(-50%);
+    height: 50vh;
+    z-index: -3;
 
-    animation: red 8s infinite linear;
-}
-
-@keyframes yellow {
-    0% {
-        top: 200px;
-        left: 100px;
-        transform: scale(1);
-    }
-
-    30% {
-        top: 300px;
-        left: 150px;
-        transform: scale(1.2);
-    }
-
-    60% {
-        top: 100px;
-        left: 200px;
-        transform: scale(1.3);
-    }
-
-    100% {
-        top: 200px;
-        left: 100px;
-        transform: scale(1);
-    }
-}
-
-@keyframes green {
-    0% {
-        top: 80px;
-        right: -20px;
-        transform: scale(1.2);
-    }
-
-    30% {
-        top: 300px;
-        right: -20px;
-        transform: scale(1);
-    }
-
-    60% {
-        top: 200px;
-        right: 100px;
-        transform: scale(1);
-    }
-
-    100% {
-        top: 80px;
-        right: -20px;
-        transform: scale(1.2);
-    }
-}
-
-@keyframes red {
-    0% {
-        top: 250px;
-        right: 0px;
-        transform: scale(1);
-    }
-
-    30% {
-        top: 150px;
-        right: 150px;
-        transform: scale(1.4);
-    }
-
-    60% {
-        top: 250px;
-        right: 100px;
-        transform: scale(1);
-    }
-
-    100% {
-        top: 250px;
-        right: 0px;
-        transform: scale(1);
-    }
+    filter: brightness(2);
 }
 </style>
--- project/app/pages/index.vue
+++ project/app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div style="height: 200vh;color: white;">
     <h1>TA MERE FIGMA</h1>
+    test
   </div>
 </template>
